8a23a1a95ae602aa932411af3730f546ea654a5d
Luan Tran <luan.tran@zien.vn>
2023-10-05 16:10:33 +0700

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 // we import our `helloWorld()` background function
-import helloWorld from "../../defer/helloWorld";
+import puppeteerJob from "../../defer/puppeteerJob";
 
 type Data = {
   ok: boolean;
@@ -12,7 +12,7 @@ export default async function handler(
 ) {
   // calling a background function triggers an execution on Defer Platform
   console.log('API :', req.headers)
-  await helloWorld("Charly");
+  await puppeteerJob();
 
   res.status(200).json({ ok: true });
 }

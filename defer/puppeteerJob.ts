81ec5d9664a44767d3ccdefc505524b816e8f3fa
Luan Tran <luan.tran@zien.vn>
2023-10-05 15:11:37 +0700

    Aaa

--- defer/puppeteerJob.ts
+++ defer/puppeteerJob.ts
@@ -1,33 +1,33 @@
-// defer/puppeteerJob.js
-import { defer } from "@defer/client";
-import puppeteer from "puppeteer";
+// // defer/puppeteerJob.js
+// import { defer } from "@defer/client";
+// import puppeteer from "puppeteer";
 
-async function helloPuppeteer() {
-  let browser;
-  try {
-    const launchConfig = {
-      headless: "new",
-      args: [
-        "--no-sandbox", // Add this flag to disable the sandbox
-        "--disable-setuid-sandbox",
-      ],
-    } as any;
+// async function helloPuppeteer() {
+//   let browser;
+//   try {
+//     const launchConfig = {
+//       headless: "new",
+//       args: [
+//         "--no-sandbox", // Add this flag to disable the sandbox
+//         "--disable-setuid-sandbox",
+//       ],
+//     } as any;
 
-    browser = (await puppeteer.launch(launchConfig)) as any;
+//     browser = (await puppeteer.launch(launchConfig)) as any;
 
-    const page = await browser.newPage();
+//     const page = await browser.newPage();
 
-    await page.goto("https://example.com");
+//     await page.goto("https://example.com");
 
-    // your logic here ...
-    console.info('DONE')
-  } catch (error) {
-    console.log(error);
-    throw error;
-  } finally {
-    await browser.close();
-  }
-}
+//     // your logic here ...
+//     console.info('DONE')
+//   } catch (error) {
+//     console.log(error);
+//     throw error;
+//   } finally {
+//     await browser.close();
+//   }
+// }
 
-// the function must be wrapped with `defer()` and exported as default
-export default defer(helloPuppeteer);
+// // the function must be wrapped with `defer()` and exported as default
+// export default defer(helloPuppeteer);
